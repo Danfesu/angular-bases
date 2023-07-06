6286ca593face7303f36af01b4869aa59ab77143
= <=>
2023-07-06 16:59:51 -0500

    dbz module

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunk',
+    power: 10
+  }];
+
+  @Output()
+  onDeleteId:EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?:string):void{
+    
+    if(!id) return;
+
+    this.onDeleteId.emit(id);
+    
+  }
+}
